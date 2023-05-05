b49bfd1aba4971df61ecae983022a88f57a973d5
HF <brentliang94@gmail.com>
2023-05-05 19:43:49 -0400

    add carousel component

--- src/layouts/HomePage/Carousel.tsx
+++ src/layouts/HomePage/Carousel.tsx
@@ -16,7 +16,7 @@ export const Carousel = () => {
                                     <img src={require('./../../Images/BooksImages/book-1000.png')}
                                         width='151'
                                         height='233'
-                                        alt="book" 
+                                        alt="book"
                                     />
                                     <h6 className='mt-2'>Book</h6>
                                     <p>Bookie</p>
@@ -32,7 +32,7 @@ export const Carousel = () => {
                                     <img src={require('./../../Images/BooksImages/book-1000.png')}
                                         width='151'
                                         height='233'
-                                        alt="book" 
+                                        alt="book"
                                     />
                                     <h6 className='mt-2'>Book</h6>
                                     <p>Bookie</p>
@@ -41,18 +41,40 @@ export const Carousel = () => {
                             </div>
                         </div>
                     </div>
-                    <button className='carousel-control-prev' type='button'
-                        data-bs-target='#carouselExampleControls' data-bs-slide='prev'>
-                        <span className='carousel-control-prev-icon' aria-hidden='true'></span>
-                        <span className='visually-hidden'>Previous</span>
-                    </button>
-                    <button className='carousel-control-next' type='button'
-                        data-bs-target='#carouselExampleControls' data-bs-slide='next'>
-                        <span className='carousel-control-next-icon' aria-hidden='true'></span>
-                        <span className='visually-hidden'>Next</span>
-                    </button>
+                </div>
+                <button className='carousel-control-prev' type='button'
+                    data-bs-target='#carouselExampleControls' data-bs-slide='prev'>
+                    <span className='carousel-control-prev-icon' aria-hidden='true'></span>
+                    <span className='visually-hidden'>Previous</span>
+                </button>
+                <button className='carousel-control-next' type='button'
+                    data-bs-target='#carouselExampleControls' data-bs-slide='next'>
+                    <span className='carousel-control-next-icon' aria-hidden='true'></span>
+                    <span className='visually-hidden'>Next</span>
+                </button>
+            </div>
+
+            {/* Mobile */}
+            <div className='d-lg-none mt-3'>
+                <div className='row d-flex justify-content-center align-items-center'>
+                    <div className='text-center'>
+                        <img src={require('./../../Images/BooksImages/book-1000.png')}
+                            width='151'
+                            height='233'
+                            alt='book'
+                        />
+                        <h6 className='mt-2'>
+                            <b>Book</b>
+                        </h6>
+                        <p>Bookie</p>
+                        <a className='btn main-color text-white' href='#'>Reserve</a>
+                    </div>
                 </div>
             </div>
+            <div className='homepage-carousel-title mt-3'>
+                <a className='btn btn-outline-secondary btn-lg' href='#'>View More</a>
+            </div>
         </div>
+
     )
 }
